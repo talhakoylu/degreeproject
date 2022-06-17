719b85c60c1e0b9e502efecc42e0b285ad242573
Ahmet Talha Köylü <ahmettalha.net@gmail.com>
2022-06-17 21:33:48 +0300

    Timer added

--- src/pages/quiz/game/game-quiz.js
+++ src/pages/quiz/game/game-quiz.js
@@ -5,6 +5,7 @@ import { Box, Button, Circle, HStack, Text, VStack } from "@chakra-ui/react";
 import { ApiService } from "@/services/api.service";
 import { useMutation, useQuery } from "react-query";
 import { useRouter } from "next/router";
+import { CountdownCircleTimer } from "react-countdown-circle-timer";
 
 
 const GameQuizPage = () => {
@@ -16,6 +17,8 @@ const GameQuizPage = () => {
     const [isQuizFinished, setIsQuizFinished] = useState(false);
     const [selectedAnswer, setSelectedAnswer] = useState('');
     const [isFinishButtonClicked, setIsFinishButtonClicked] = useState(false);
+    const [isLoading, setIsLoading] = useState(false);
+    const [restartTimer, setRestartTimer] = useState(0);
 
     useEffect(() => {
         // localStorage.setItem('gameData', JSON.stringify({"quizId":"62a23d25f8198e162f7a2bd7","gameId":"62a8545e54adf2fcfe3e73af","resultId":"62a8546754adf2fcfe3e73b7"}))
@@ -28,30 +31,38 @@ const GameQuizPage = () => {
 
     useEffect(() => {
         if (getQuestionsQuery.isSuccess) {
-            setQuestionData(getQuestionsQuery.data.data.data[currentQuestionNumber]);
             if (currentQuestionNumber > getQuestionsQuery.data.data.data.length - 1) {
                 setIsQuizFinished(true);
-                setResultId(gameData.resultId)
+                setResultId(gameData.resultId);
                 localStorage.removeItem('gameData');
                 localStorage.removeItem('currentQuestionNumber');
                 setGameData(undefined);
                 setQuestionData(undefined);
+            } else {
+                setQuestionData(getQuestionsQuery.data.data.data[currentQuestionNumber]);
             }
         }
     }, [getQuestionsQuery.isSuccess, currentQuestionNumber]);
 
     const onSubmitSave = async () => {
-        const answer = questionData;
-        answer.selectedAnswer = selectedAnswer === "-" ? "" : selectedAnswer;
-        answer.questionId = answer._id;
-        delete answer._id;
-        await submitAnswerMutation.mutateAsync({ resultId: gameData.resultId, data: answer });
+        if (!isLoading) {
+            await setIsLoading(true);
+            const answer = {
+                ...questionData,
+                questionId: questionData._id,
+                _id: undefined,
+                selectedAnswer: selectedAnswer === "-" ? "" : selectedAnswer
+            };
+            await submitAnswerMutation.mutateAsync({ resultId: gameData.resultId, data: answer });
+            await setIsLoading(false);
+        }
     };
 
     useEffect(() => {
         if (submitAnswerMutation.isSuccess) {
             localStorage.setItem('currentQuestionNumber', parseInt(currentQuestionNumber) + 1);
             setCurrentQuestionNumber(localStorage.getItem("currentQuestionNumber"));
+            setRestartTimer(prev => prev + 1);
             setSelectedAnswer('');
             console.log("data", submitAnswerMutation.data);
         }
@@ -60,7 +71,7 @@ const GameQuizPage = () => {
 
 
     const onSubmitFinish = () => {
-        setResultId(gameData.resultId)
+        setResultId(gameData.resultId);
         localStorage.removeItem('gameData');
         localStorage.removeItem('currentQuestionNumber');
         setGameData(undefined);
@@ -77,9 +88,23 @@ const GameQuizPage = () => {
             <Box marginX={"auto"} bg={"white"} rounded={"md"} minW={{ base: "full", md: 800 }} overflow={"hidden"}>
                 {getQuestionsQuery.isFetching && <Text>Yükleniyor...</Text>}
                 {(getQuestionsQuery.isSuccess && questionData) && <VStack spacing={6} width={"full"} px={4} py={3}>
-                    <HStack spacing={0} width={"full"} justifyContent={"center"} alignItems={{ base: "start", md: "stretch" }} flexDirection={{ base: "column-reverse", md: "row" }}>
+                    <HStack spacing={0} width={"full"} justifyContent={"center"} alignItems={'center'} flexDirection={{ base: "column-reverse", md: "row" }}>
                         <Box width={"full"} fontWeight={"bold"}>Question {parseInt(currentQuestionNumber) + 1}</Box>
-                        <Box minW={"15%"} height={"auto"} textAlign={"right"}>Kalan Süre: {questionData.timer}</Box>
+                        <HStack minW={"15%"}>
+                            <Text>Kalan Süre: </Text>
+                            <CountdownCircleTimer
+                                key={restartTimer}
+                                isPlaying={true}
+                                duration={questionData.timer}
+                                strokeWidth={6}
+                                colors={['#004777', '#F7B801', '#A30000', '#A30000']}
+                                colorsTime={[questionData.timer / 1.5, questionData.timer / 2, questionData.timer / 4, 0]}
+                                size={50}
+                                onComplete={() => onSubmitSave()}
+                            >
+                                {({ remainingTime }) => remainingTime}
+                            </CountdownCircleTimer>
+                        </HStack>
                     </HStack>
 
                     <Box rounded={"md"} px={6} py={4} bg={"purple.100"} width={"full"}>
